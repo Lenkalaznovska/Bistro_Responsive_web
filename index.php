88fa02411a4fa42ca61c89075c9ba75862541aaa
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-08-06 20:23:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -260,5 +260,18 @@
             }
         });
     </script>
+    
+    <!-- Chat Tawk.to -->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/65dcd4538d261e1b5f65849b/1hnj9qq2j';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
 </body>
 </html>
